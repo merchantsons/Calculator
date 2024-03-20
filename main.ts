0e18463dc9f4d6cee17160aa7bbb3e7615ecc3e8
merchantsons <160190585+merchantsons@users.noreply.github.com>
2024-03-21 02:17:21 +0500

    Update

---
+++ main.ts
@@ -0,0 +1,23 @@
+import inquirer from 'inquirer';
+
+const takeanswer = await inquirer.prompt([
+    { message: 'Enter first number', type: 'number', name: 'FirstNumber'},
+    { message: 'Enter second number', type: 'number', name: 'SecondNumber'},
+    {
+        message: 'Select Operator ?',
+        type: 'list',
+        name: 'operator',
+        choices: ['Addition','Subtraction','Multiplication','Division'],
+    },
+]);
+
+if (takeanswer.operator == 'Addition')
+   {console.log(takeanswer.FirstNumber + takeanswer.SecondNumber);}
+else if (takeanswer.operator == 'Subtraction')
+   {console.log(takeanswer.FirstNumber - takeanswer.SecondNumber);}
+else if (takeanswer.operator == 'Multiplication')
+   {console.log(takeanswer.FirstNumber * takeanswer.SecondNumber);}
+else if (takeanswer.operator == 'Division')
+   {console.log(takeanswer.FirstNumber / takeanswer.SecondNumber);}
+else
+   {console.log('Kindly Select Valid Operator')};
